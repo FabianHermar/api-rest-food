30f3f004a97bc1c55bc7f611332e03ba1954f64f
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-13 17:55:43 -0600

    chore: Update express server to include food route

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from 'express'
+import morgan from 'morgan'
+
+import FoodRoute from './routes/FoodRoute.js'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' })
+app.use( express.urlencoded( { extended: false } ) )
+app.use( express.json() )
+app.use( morgan( 'dev' ) )
+
+app.use( '/api', FoodRoute )
+
+app.get( ( req, res ) => {
+  res.status( 404 ).json( { message: 'Not found' } )
 })
 
 app.listen(port, () => {
